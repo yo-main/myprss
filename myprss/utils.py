f95ff6e99bc51fbff350ae88e524459e367a9b92
rarnal <arnal.romain@gmail.com>
2020-05-10 13:05:18 +0200

    fix: bug with the list_known_rss_feeds autocompletion function

--- myprss/utils.py
+++ myprss/utils.py
@@ -1,6 +1,6 @@
 from myprss.config import Config
 
 
-def list_known_feeds(ctx):
+def list_known_feeds(ctx, args, incomplete):
     config = Config()
-    return list(config["registry"].keys())
+    return list(config.data["registry"].keys())
